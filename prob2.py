a63f395e4b771be674849af5b6c68de7600ea435
Jerald Torres <115351172+AkosiJerald@users.noreply.github.com>
2023-07-20 14:18:16 +0800

    Add files via upload

---
+++ prob2.py
@@ -0,0 +1,15 @@
+input = (input("Enter text: "))
+
+class Solution:
+    def solve(self, s, k):
+        def shift (c):
+            i =ord(c) - ord('a')
+            i += k
+            i %= 26
+            return chr(ord('a') + i)
+
+        return "".join(map(shift, s))
+
+ob = Solution()
+print(ob.solve(input, 3))
+
